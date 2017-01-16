49c4eadc83de93b01ac17d9e14693970abd86bdf
Ryne Cheow <rynecheow@gmail.com>
2017-01-16 09:58:21 +0800

    UITest target
    Signed-off-by: Ryne Cheow <rynecheow@gmail.com>

---
+++ PopupKitExample/Pods/Target Support Files/Pods-PopupKit_ObjC_Example-PopupKit_ObjC_ExampleUITests/Pods-PopupKit_ObjC_Example-PopupKit_ObjC_ExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PopupKit_ObjC_Example_PopupKit_ObjC_ExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PopupKit_ObjC_Example_PopupKit_ObjC_ExampleUITests
+@end
---
+++ PopupKitExample/Pods/Target Support Files/Pods-PopupKit_ObjC_Example-PopupKit_ObjC_ExampleUITests/Pods-PopupKit_ObjC_Example-PopupKit_ObjC_ExampleUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_PopupKit_ObjC_Example_PopupKit_ObjC_ExampleUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PopupKit_ObjC_Example_PopupKit_ObjC_ExampleUITestsVersionString[];
+
